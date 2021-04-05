855c9cdc8598402ed244dc199a27a57030414d8f
domihorinka <domihorinka@gmail.com>
2021-04-05 12:20:45 -0600

    created new repo for portfolio to fix error

---
+++ src/resumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+    "imagebaseurl":" https://github.com/domihorinka",
+    "name": "Dominique Horinka",
+    "role": "Junior Web Developer",
+    "linkedinId":"https://www.linkedin.com/in/dominique-horinka/",
+    "skypeid": "",
+    "roleDescription": " I love the challenge of coding.  I have an absolute thirst for gaining knowledge on all things web development.  I hope to continue to enrich my knowledge gained from past endeavors through future work experience. ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/dominique-horinka/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/domihorinka",
+          "className":"fa fa-github"
+        }
+
+      ],
+    "aboutme":"I recently graduated from the University of Denver's Full Stack Flex coding boot camp.  I have a passion for learning and solving complex issues.  Coding has given me an outlet to practice both.",
+    "address":"Denver, CO",
+    "cell": "(908)635-8964",
+    "website":"domihorinka@gmail.com",
+    "education":[
+      {
+        "UniversityName":"University of Denver Coding Bootcamp",
+        "specialization":"Full Stack Flex Web Development",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2021",
+        "Achievements":"Completed the full-time program that covered full stack, including HTML5, CSS3, JavaScript, jQuery, Node.js, Express.js, React.js, Database Theory, MongoDB, MySQL, Command Line, Git, and more."
+      },
+      {
+        "UniversityName":"Watchung Hill Regional High School",
+        "specialization":"High School Diploma",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2016",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Domi's Dogs",
+        "specialization":"Business Operator",
+        "MonthOfLeaving":"December",
+        "YearOfLeaving":"2020",
+        "Achievements":"Sole owner and organizer of business operations, coordinating and scheduling, handling financial aspect, and assuming responsibility of client's pets and sometimes homes."
+      },
+      {
+        "CompanyName":"Teakoe Tea",
+        "specialization":"Brand Ambassador/Sales",
+        "MonthOfLeaving":"December",
+        "YearOfLeaving":"2018",
+        "Achievements":"Built and kept a positive relationship with clients, introduced product into new stores, conducted market research by identifying emerging store trends, and kept management informed by submitting activity and results reports. "
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Brawndo - A water tracking app!",
+        "description":"mobileapp",
+        "url": "https://brawndoplant.herokuapp.com/garden",
+        "imgurl":"images/portfolio/phone.jpg",
+      },
+      {
+        "name":"Cobra's Calorie Counter",
+        "description":"mobileapp",
+        "url": "https://codingcobrascoding.github.io/CobrasCalorieCounter/",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Hall of Heroes - DND Character Sheets",
+        "description":"mobileapp",
+        "url": "https://hall-of-heroes.herokuapp.com/",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Note Taker App",
+        "description":"mobileapp",
+        "url": "https://github.com/domihorinka/NoteTaker",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Burger Eating App",
+        "description":"mobileapp",
+        "url":"https://burgereatin.herokuapp.com/",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"Fitness Tracker",
+        "description":"mobileapp",
+        "url":"https://track-it-fitness.herokuapp.com/",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ]
+  }
+  
+  export default resumeData
