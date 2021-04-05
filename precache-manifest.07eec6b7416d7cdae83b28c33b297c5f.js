adaf16dcb8cf5038ce1304532a2590d5ce658102
domihorinka <domihorinka@gmail.com>
2021-04-05 14:06:07 -0600

    Updates

---
+++ precache-manifest.07eec6b7416d7cdae83b28c33b297c5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a1453923e6e5b1ad8d9",
+    "url": "/React-Portfolio/static/css/main.33d47abf.chunk.css"
+  },
+  {
+    "revision": "2a1453923e6e5b1ad8d9",
+    "url": "/React-Portfolio/static/js/main.2a145392.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/React-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "634029f668ad1d2a2290e847aa7ffce9",
+    "url": "/React-Portfolio/index.html"
+  }
+];
